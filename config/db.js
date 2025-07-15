23e160f5502f649f275c49a9f2a98b6781a6c76d
Akshat3128 <akshatsaxena62941@gmail.com>
2025-07-16 01:14:06 +0530

    updated db for deployment

--- config/db.js
+++ config/db.js
@@ -3,16 +3,16 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     
-  process.env.DB_USER,     
-  process.env.DB_PASS, 
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 
 export default sequelize;
